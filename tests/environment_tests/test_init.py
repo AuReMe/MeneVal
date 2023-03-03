b83da927e97c9ffc1f3211c2520e03ffe49ee4ff
phamongi <octavia.phg@gmail.com>
2023-03-03 14:57:09 +0100

    test init

--- tests/environment_tests/test_init.py
+++ tests/environment_tests/test_init.py
@@ -38,20 +38,20 @@ class Test(unittest.TestCase):
 
     @staticmethod
     def move_files():
-        source_loc = [('../Datas/AuCoMe/group_template.tsv', 'Input/AuCoMe/group_template.tsv'),
-                      ('../Datas/AuCoMe/reactions.tsv', 'Input/AuCoMe/reactions.tsv'),
-                      ('../Datas/DataBase/metacyc_26.0_prot70.padmet', 'Input/DataBase/metacyc_26.0_prot70.padmet'),
-                      ('../Datas/DataBase/proteins_seq_ids_reduced_70.fasta',
-                       'Input/DataBase/proteins_seq_ids_reduced_70.fasta'),
-                      ('../Datas/Networks/CFT073.padmet', 'Input/Networks/CFT073.padmet'),
-                      ('../Datas/Seeds/artefacts.tsv', 'Input/Seeds/artefacts.tsv'),
-                      ('../Datas/Seeds/seeds.tsv', 'Input/Seeds/seeds.tsv'),
-                      ('../Datas/Species_seq/CFT073/CFT073.faa', 'Input/Species_seq/CFT073.faa'),
-                      ('../Datas/Species_seq/CFT073/CFT073.fna', 'Input/Species_seq/CFT073.fna'),
-                      ('../Datas/Targets/targets.tsv', 'Input/Targets/targets.tsv')
-                      ]
-        for pair in source_loc:
-            shutil.copy(pair[0], pair[1])
+        source_dest = {'../Datas/AuCoMe/group_template.tsv': 'Input/AuCoMe/group_template.tsv',
+                       '../Datas/AuCoMe/reactions.tsv': 'Input/AuCoMe/reactions.tsv',
+                       '../Datas/DataBase/metacyc_26.0_prot70.padmet': 'Input/DataBase/metacyc_26.0_prot70.padmet',
+                       '../Datas/DataBase/proteins_seq_ids_reduced_70.fasta':
+                           'Input/DataBase/proteins_seq_ids_reduced_70.fasta',
+                       '../Datas/Networks/CFT073.padmet': 'Input/Networks/CFT073.padmet',
+                       '../Datas/Seeds/artefacts.tsv': 'Input/Seeds/artefacts.tsv',
+                       '../Datas/Seeds/seeds.tsv': 'Input/Seeds/seeds.tsv',
+                       '../Datas/Species_seq/CFT073/CFT073.faa': 'Input/Species_seq/CFT073.faa',
+                       '../Datas/Species_seq/CFT073/CFT073.fna': 'Input/Species_seq/CFT073.fna',
+                       '../Datas/Targets/targets.tsv': 'Input/Targets/targets.tsv'
+                       }
+        for source, destination in source_dest.items():
+            shutil.copy(source, destination)
 
     def test_check(self):
         self.move_files()
